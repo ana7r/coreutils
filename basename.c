70b02ca8c3de46c6af85d464787a73d5a114e711
ana7r <138595088+ana7r@users.noreply.github.com>
2023-09-08 12:52:55 +0300

    add basename command

---
+++ basename.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2023 Mohamed Abdelhamed
+ */
+
+#include <libgen.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void remove_suffix(char *name, char *suffix) {
+  // basename.cpp
+  //            ^
+  //         ^
+  char *np = name + strlen(name);
+  char *sp = np - strlen(suffix);
+
+  char *sp_new = strstr(sp, suffix);
+  if (sp_new)
+    *sp_new = '\0';
+}
+
+void usage(int argc, char *argv[]) {
+  fprintf(stdout, "Usage: %s NAME [SUFFIX]\n", argv[0]);
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    fprintf(stderr, "%s: missing operand\n", argv[0]);
+    fprintf(stderr, "Try '%s --help' for more information.\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  char *c = basename(argv[1]);
+  if (argc > 2)
+    remove_suffix(c, argv[2]);
+
+  fprintf(stdout, "%s\n", c);
+  return EXIT_SUCCESS;
+}
